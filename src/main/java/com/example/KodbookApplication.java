f5b51ba37dba6d1c9766fac4dae67964846df702
Varun B L <hitmanrohit000@gmail.com>
2024-10-17 15:12:51 +0530

    commit

---
+++ src/main/java/com/example/KodbookApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodbookApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/services/UserService.java
@@ -0,0 +1,17 @@
+package com.example.services;
+
+import com.example.entities.User;
+
+public interface UserService {
+
+	void addUser(User user);
+
+	boolean userExists(String username, String email);
+
+	boolean validateUser(String username, String password);
+
+	User getUser(String username);
+
+	void updateUser(User user);
+
+}
